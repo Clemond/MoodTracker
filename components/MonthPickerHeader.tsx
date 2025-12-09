2be80d674d181f31312d0fc6fd5091db6c69382b
Nicholas <quackshows11@gmail.com>
2025-12-09 12:19:55 +0100

    feat: add real current date in the MonthPickerHeader

--- components/MonthPickerHeader.tsx
+++ components/MonthPickerHeader.tsx
@@ -2,13 +2,16 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Icon } from "react-native-paper";
 
 export default function MonthPickerHeader() {
+  const currentDate = new Date();
+  const month = currentDate.toLocaleString("default", { month: "long" });
+
   return (
     <View>
       <View style={styles.monthHeader}>
         <TouchableOpacity>
           <Icon source={"chevron-left"} size={30} />
         </TouchableOpacity>
-        <Text style={styles.monthText}>December</Text>
+        <Text style={styles.monthText}>{month}</Text>
         <TouchableOpacity>
           <Icon source={"chevron-right"} size={30} />
         </TouchableOpacity>
