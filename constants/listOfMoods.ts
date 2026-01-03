6bc94d94d8cc1fe738ef6be23bae76bea4dc429f
Nicholas <quackshows11@gmail.com>
2026-01-03 15:59:48 +0100

    feat: give IMoodButton an id

--- constants/listOfMoods.ts
+++ constants/listOfMoods.ts
@@ -1,10 +1,10 @@
 import { IMoodButton } from "../types/IMoodButton";
 
 export const listOfMoods: IMoodButton[] = [
-  { feeling: "Happy", icon: require("../assets/happy.png") },
-  { feeling: "Angry", icon: require("../assets/angry.png") },
-  { feeling: "Sad", icon: require("../assets/sad.png") },
-  { feeling: "Anxious", icon: require("../assets/anxious.png") },
-  { feeling: "Depressed", icon: require("../assets/depressed.png") },
-  { feeling: "Embarrassed", icon: require("../assets/embarrassed.png") }
+  { id: 1, feeling: "Happy", icon: require("../assets/happy.png") },
+  { id: 2, feeling: "Angry", icon: require("../assets/angry.png") },
+  { id: 3, feeling: "Sad", icon: require("../assets/sad.png") },
+  { id: 4, feeling: "Anxious", icon: require("../assets/anxious.png") },
+  { id: 5, feeling: "Depressed", icon: require("../assets/depressed.png") },
+  { id: 6, feeling: "Embarrassed", icon: require("../assets/embarrassed.png") }
 ];
