47ce3b28182153fe827e546c69059204795af50d
Nicholas <quackshows11@gmail.com>
2026-01-03 13:49:00 +0100

    feat: create CheckBox component

--- components/BottomHSPart.tsx
+++ components/BottomHSPart.tsx
@@ -1,10 +1,12 @@
 import { View, StyleSheet } from "react-native";
 import MonthPickerHeader from "./MonthPickerHeader";
+import CheckBox from "./grid/CheckBox";
 
 export default function BottomHSPart() {
   return (
     <View style={styles.bottomPart}>
       <MonthPickerHeader />
+      <CheckBox />
     </View>
   );
 }
