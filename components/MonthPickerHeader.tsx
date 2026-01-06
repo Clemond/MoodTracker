850a88a1e79941a51231f13a269ead94af667cbb
Nicholas <quackshows11@gmail.com>
2026-01-06 14:35:31 +0100

    feat: implement so that the user can switch between months in the picker

--- components/MonthPickerHeader.tsx
+++ components/MonthPickerHeader.tsx
@@ -1,32 +1,53 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Icon } from "react-native-paper";
 import { useCurrentDateStore } from "../stores/useCurrentDateStore";
+import { useEffect, useState } from "react";
 
 export default function MonthPickerHeader() {
   const { currentDate } = useCurrentDateStore();
-  const currentMonthString = currentDate.toLocaleString("default", {
+  const [userChoiceMonth, setUserChoiceMonth] = useState<number>(0);
+  const currentMonthString = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + userChoiceMonth
+  ).toLocaleString("default", {
     month: "long"
   });
-  const currentMonthInt = parseInt(
-    currentDate.toLocaleString("default", {
-      month: "numeric"
-    })
-  );
+
+  const [displayedMonth, setDisplayedMonth] =
+    useState<string>(currentMonthString);
+
+  useEffect(() => {
+    setDisplayedMonth(currentMonthString);
+  }, [userChoiceMonth]);
+
+  // * This code block will go in the grid files later *
+
+  const currentMonthInt =
+    parseInt(
+      currentDate.toLocaleString("default", {
+        month: "numeric"
+      })
+    ) + userChoiceMonth;
 
   const totalDaysInMonth = new Date(
     currentDate.getFullYear(),
     currentMonthInt,
     0
   ).getDate();
+  //* End of code block *
 
   return (
     <View>
       <View style={styles.monthHeader}>
-        <TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setUserChoiceMonth(userChoiceMonth - 1)}
+        >
           <Icon source={"chevron-left"} size={30} />
         </TouchableOpacity>
-        <Text style={styles.monthText}>{currentMonthString}</Text>
-        <TouchableOpacity>
+        <Text style={styles.monthText}>{displayedMonth}</Text>
+        <TouchableOpacity
+          onPress={() => setUserChoiceMonth(userChoiceMonth + 1)}
+        >
           <Icon source={"chevron-right"} size={30} />
         </TouchableOpacity>
       </View>
